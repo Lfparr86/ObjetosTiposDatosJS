a18aeafba648d7caf912d43c79db8daf350e185b
lfparr86 <luisfernando.parravaquerizo@gmail.com>
2022-11-10 13:59:35 +0100

    Actualización 5

---
+++ 006calculator/006calculator.js
@@ -0,0 +1,13 @@
+"use strict"
+
+let numeroA = +prompt("Introduce el primer numero");
+
+let numeroB = +prompt("Introduce el segundo numero");
+
+let numeros={};
+
+numeros.numeroA=numeroA;
+
+numeros.numeroB=numeroB;
+
+//alert(Object.values(numeros));estan dentro.
