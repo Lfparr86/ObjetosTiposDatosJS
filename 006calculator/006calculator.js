5ddb724a7f781085036df58da5f20fc7668d440a
lfparr86 <luisfernando.parravaquerizo@gmail.com>
2022-11-12 18:14:39 +0100

    Actualizado 6

--- 006calculator/006calculator.js
+++ 006calculator/006calculator.js
@@ -1,13 +1,47 @@
 "use strict"
+/*
+006calculator.js/.html: Crea un objeto calculator con tres métodos:
+read() pide dos valores y los almacena como propiedades de objeto con nombres a y b.
+sum() devuelve la suma de los valores almacenados.
+mul() multiplica los valores almacenados y devuelve el resultado.
+let calculator = {
+  // ... tu código ...
+};
+calculator.read();
+alert( calculator.sum() );
+alert( calculator.mul() );
+*/
 
-let numeroA = +prompt("Introduce el primer numero");
 
-let numeroB = +prompt("Introduce el segundo numero");
 
-let numeros={};
+let calculator={//tres funciones dentro del ojbeto recordar la coma ,
 
-numeros.numeroA=numeroA;
+        //numeroA : +prompt("Introduce el primer numero"),podemos hacer la llamada directamente
+        //numeroB : +prompt("Introduce el segundo numero"),
 
-numeros.numeroB=numeroB;
+    read(){
 
-//alert(Object.values(numeros));estan dentro.
+        this.numeroA = +prompt("Introduce el primer numero");
+
+        this.numeroB = +prompt("Introduce el segundo numero");
+    },
+
+    sum(){
+        
+        this.resultado = this.numeroA + this.numeroB;//this pera llamar pero NaN
+        return this.resultado;
+    },
+
+    mul(){
+        
+        this.resultado = this.numeroA * this.numeroB;//this pera llamar pero NaN
+        return this.resultado;
+    }
+
+};
+
+//calculator.numeroA;
+//calculator.numeroB;
+calculator.read();//llamamos al objeto calculator que tiene la funcion read.
+alert( calculator.sum() );//llamamos a la funcion
+alert( calculator.mul() );
