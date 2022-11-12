0354b9d665c10c69985b668a4d90c486199e7b34
lfparr86 <luisfernando.parravaquerizo@gmail.com>
2022-11-12 18:47:39 +0100

    Actualizado 7

---
+++ 007ladder/007ladder.js
@@ -0,0 +1,45 @@
+"use strict"
+
+/*
+007ladder.js/.html: Hay un objeto ladder que permite subir y bajar.
+let ladder = {
+  step: 0,
+  up() {
+    this.step++;
+  },
+  down() {
+    this.step--;
+  },
+  showStep: function() { // muestra el peldaño actual
+    alert( this.step );
+  }
+};
+Ahora, si necesitamos hacer varios llamados en secuencia podemos hacer algo como 
+esto:
+ladder.up();
+ladder.up();
+ladder.down();
+ladder.showStep(); // 1
+ladder.down();
+ladder.showStep(); // 0
+Modifica el código de “arriba” up, “abajo” down y “mostrar peldaño” showStep para 
+hacer los llamados encadenables como esto:
+ladder.up().up().down().showStep().down().showStep(); // shows 1 then 0
+Tal enfoque es ampliamente usado entre las librerías JavaScript.
+*/
+
+let ladder = {
+    step: 0,
+    up() {
+      return this.step++;
+    },
+    down() {
+      this.step--;
+    },
+    showStep: function() { // muestra el peldaño actual
+      alert( this.step );
+    }
+  };
+
+ alert( ladder.up());//undefined? Recordar poner return
+ alert( ladder.up());//undefined?
