2d4239718f237a9b09a0b349d58d3951948197d2
lfparr86 <luisfernando.parravaquerizo@gmail.com>
2022-11-14 13:59:34 +0100

    Actualizar 11

---
+++ 011addPropertyToString/011addPropertyToString.js
@@ -0,0 +1,14 @@
+"use strict"
+
+/*
+011addPropertyToString.js/.html: ¿Puedo agregar una propiedad a un string?: 
+Considera el siguiente código:
+let str = "Peter";
+str.test = 5;
+alert(str.test);
+Qué piensas: ¿funcionará? ¿Qué mostrará? ¿Por qué?
+*/
+
+let str = "Peter";
+str.test = 5;
+alert(str.test);
